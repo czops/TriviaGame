022be87f1d2a26b0a5e7cb8e285c4a1e2a29cf46
czops <37029225+czops@users.noreply.github.com>
2019-01-13 14:51:08 -0600

    tried to use object notation for the questions and answers but getting nowhere

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,74 +1,93 @@
 //Variables
 
 var Questions = {
-    Q1 ={
+    Q1: {
         question:  "What is the capital of Ukraine?",
         answer1: "Kiev",
         answer2: "Warsaw",
         answer3: "Paris",
         answer4:  "Bratislava"
     },
-    Q2 ={
+    Q2: {
         question:  "In what city would you expect to see pretzels being sold by street vendors in October?",
         answer1: "London",
         answer2: "Reykjavik",
         answer3: "Buenos Aires",
         answer4:  "Munich"
     },
-    Q3 ={
+    Q3: {
         question:  "In 1949, the international community gathered in this city to ratify basic protections for citizens of the world living in a state of war...",
         answer1: "The Hague",
         answer2: "Zurich",
         answer3: "Geneva",
         answer4:  "Berlin"
     },
-    Q4 ={
+    Q4: {
         question:  "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4:  ""
-    },
-    Q5 ={
-        question:  "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4:  ""
-    },
-    Q6 ={
-        question: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4:  ""
-    },
-    Q7 ={
-        question: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4:  ""
-    },
-    Q8 ={
-        question: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4:  ""
-    },
-    Q9 ={
-        question: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4:  ""
-    },
-    Q10 ={
-        question: "",
         answer1: "",
         answer2: "",
         answer3: "",
         answer4:  ""
     }
+    // Q5: {
+    //     question:  "",
+    //     answer1: "",
+    //     answer2: "",
+    //     answer3: "",
+    //     answer4:  ""
+    // },
+    // Q6: {
+    //     question: "",
+    //     answer1: "",
+    //     answer2: "",
+    //     answer3: "",
+    //     answer4:  ""
+    // },
+    // Q7: {
+    //     question: "",
+    //     answer1: "",
+    //     answer2: "",
+    //     answer3: "",
+    //     answer4:  ""
+    // },
+    // Q8: {
+    //     question: "",
+    //     answer1: "",
+    //     answer2: "",
+    //     answer3: "",
+    //     answer4:  ""
+    // },
+    // Q9: {
+    //     question: "",
+    //     answer1: "",
+    //     answer2: "",
+    //     answer3: "",
+    //     answer4:  ""
+    // },
+    // Q10: {
+    //     question: "",
+    //     answer1: "",
+    //     answer2: "",
+    //     answer3: "",
+    //     answer4:  ""
+    //}
 };
+
+console.log(Questions.Q1.question)
+$("#add-question").html(
+    '<h4> ${Questions.Q1.question} </h4>'
+);
+
+function attachQuestions(){
+    Object.keys(Questions).forEach(function(element) {
+        debugger;
+        console.log(element.question);
+        
+        debugger;
+        $("#add-question").html(
+            '<h4> ${Questions.Q1.question} </h4>'
+        
+    );
+};
+
+attachQuestions();
