d7c649d80e3384c26e7d910126680d3f013d5941
czops <37029225+czops@users.noreply.github.com>
2019-01-14 17:43:19 -0600

    can't seem to make my inputs into radios..

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,15 +3,19 @@
 var Questions = {
     Q1: "What is the capital of Ukraine?",
     Q2: "In what city would you expect to see pretzels being sold by street vendors in October?",
-    Q3: "In 1949, the international community gathered in this city to ratify basic protections for citizens of the world living in a state of war..."
-
+    Q3: "In 1949, the international community gathered in this city to ratify basic protections for citizens of the world living in a state of war...",
+    Q4: "If you see the biggest ferris wheel you've ever seen in your life, you're probably in this city...",
+    Q5: "Which of these is a sister city of Minneapolis? It is at approximately the same latitude..."
 };
 
-var Answers = ["Kiev", "Warsaw", "Paris", "Bratislava", "London", 
-"Reykjavik", "Buenos Aires", "Munich", 
-"The Hague", "Zurich", "Geneva", "Berlin"];
+var Answers = ["Kiev", "Warsaw", "Paris", "Bratislava", 
+"London", "Reykjavik", "Buenos Aires", "Munich", 
+"The Hague", "Zurich", "Geneva", "Berlin",
+"Rio De Janeiro", "London", "New York City", "Las Vegas",
+"Lima", "Harbin", "Portland", "Osaka"
+];
 
-
+//I want to do it this way, but I don't have time to mess around with it anymore...
 var QuestionsArray = [
     {
       question: "What is the capital of Ukraine?",
@@ -57,11 +61,12 @@ window.onload = function () {
 
 
 
-//  Variable that will hold our setInterval that runs the stopwatch
+//  this is for the interval variable that's set in the start game function
 var intervalId;
 
 // prevents the clock from being sped up unnecessarily
 var clockRunning = false;
+//give people 60 seconds
 var time = 60;
 x = 0;
 
@@ -70,7 +75,7 @@ function reset() {
     time = 60;
 
     //  TODO: Change the "display" div to "00:00."
-    $('#display').html("00:00");
+    $('#display').html("00:60");
 
 }
 
@@ -91,7 +96,7 @@ function count() {
     TimeDisplay = timeConverter(time);
     $('#display').html(TimeDisplay);
     //  TODO: Use the variable you just created to show the converted time in the "display" div.
-    if (time = 0) {
+    if (time === 0) {
         EndGame();
         SubmitButton();
     }
@@ -129,21 +134,26 @@ function timeConverter(t) {
 //     '<h4> ${Questions.Q1.question} </h4>'
 // );
 
+
+//Wasted a TON of time trying to code this section differently.
+
 function StartGame() {
 
     Object.keys(Questions).forEach(function (element) {
-        debugger;
+        // debugger;
         $("#add-question").append("<form></form>")
         $("#add-question").append( "<h4>" + Questions[element] + "</h4>");
 
         //Also need to append the answers
 
-        debugger;
+        // debugger;
         for (i = 0; i < 4; i++) {
             $("#add-question").append("<input>" + Answers[i + x] + "</input>");
             $('<input type="radio">');
         };
         x += 4;
+
+        $("#add-question").append( "<br><br>");
 
     });
 
@@ -152,16 +162,15 @@ function StartGame() {
 
 
     //generate the submit button once the questions are all on the page
-    if (indexOf(Questions[element] === Questions.length)) {
+    // if (indexOf(Questions[element] === Questions.length)) {
 
-        $("#add-question").append("<button>" + "Submit Answer" + "</button>")
+    //     $("#add-question").append("<button>" + "Submit Answer" + "</button>")
 
-    };
 
-    
-    if (!clockRunning) {
+    if (x=16 && !clockRunning) {
         intervalId = setInterval(count, 1000);
     };
+
 };
 
 function SubmitButton() {
